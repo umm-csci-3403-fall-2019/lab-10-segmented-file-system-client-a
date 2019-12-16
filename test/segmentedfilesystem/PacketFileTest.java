7af3f8b7a1ec5dbdee0c66daea82a17834004546
Paul Friederichsen <floogulinc@gmail.com>
2019-12-16 02:24:23 -0600

    Add a few more junit tests

--- test/segmentedfilesystem/PacketFileTest.java
+++ test/segmentedfilesystem/PacketFileTest.java
@@ -7,11 +7,6 @@ import com.google.common.primitives.Shorts;
 
 import org.junit.Test;
 
-/**
- * This is just a stub test file. You should rename it to
- * something meaningful in your context and populate it with
- * useful tests.
- */
 public class PacketFileTest {
 
     @Test
@@ -30,7 +25,7 @@ public class PacketFileTest {
         boolean result = testFile.addPacket((byte) 0, fileID, new byte[] {0, fileID, 'T', 'e', 's', 't'}, 6);
         assertFalse("result of addPacket should be false", result);
         assertTrue("hasHeader should be set", testFile.hasHeader);
-        assertEquals("filename not correct", "Test", testFile.fileName);
+        assertEquals("filename should be correct", "Test", testFile.fileName);
         assertFalse("hasEndPacket should not be set", testFile.hasEndPacket);
     }
 
@@ -44,7 +39,31 @@ public class PacketFileTest {
         assertFalse("hasHeader should not be set", testFile.hasHeader);
         assertEquals("file length should be correct", 6, testFile.fileLength);
         assertEquals("parts size should be 1", 1, testFile.parts.size());
-        assertArrayEquals(new byte[] {1,5,3}, testFile.parts.get((short) 5));
+        assertArrayEquals("part data should be correct", new byte[] {1,5,3}, testFile.parts.get((short) 5));
+    }
+
+    public void testDataPacket() {
+        byte fileID = 12;
+        PacketFile testFile = new PacketFile(fileID);
+        boolean result = testFile.addPacket((byte) 1, fileID, Bytes.concat(new byte[] {(byte) 1, fileID}, Shorts.toByteArray((short) 5), new byte[] {2,5,4,6,7,1,2,3}), 12);
+        assertFalse("result of addPacket should be false", result);
+        assertFalse("hasEndPacket should not be set", testFile.hasEndPacket);
+        assertFalse("hasHeader should not be set", testFile.hasHeader);
+        assertEquals("parts size should be 1", 1, testFile.parts.size());
+        assertArrayEquals("part data should be correct", new byte[] {2,5,4,6,7,1,2,3}, testFile.parts.get((short) 5));
+    }
+
+    public void testMultipleDataPacket() {
+        byte fileID = 12;
+        PacketFile testFile = new PacketFile(fileID);
+        testFile.addPacket((byte) 1, fileID, Bytes.concat(new byte[] {(byte) 11, fileID}, Shorts.toByteArray((short) 1), new byte[] {1,5,3}), 7);
+        boolean result = testFile.addPacket((byte) 1, fileID, Bytes.concat(new byte[] {(byte) 1, fileID}, Shorts.toByteArray((short) 0), new byte[] {2,5,4,6,7,1,2,3}), 12);
+        assertFalse("result of addPacket should be false", result);
+        assertFalse("hasEndPacket should not be set", testFile.hasEndPacket);
+        assertFalse("hasHeader should not be set", testFile.hasHeader);
+        assertEquals("parts size should be 2", 2, testFile.parts.size());
+        assertArrayEquals("part data should be correct", new byte[] {1,5,3}, testFile.parts.get((short) 1));
+        assertArrayEquals("part data should be correct", new byte[] {2,5,4,6,7,1,2,3}, testFile.parts.get((short) 0));
     }
 
 }
